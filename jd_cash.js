71a4c59539474fa5b041007935805b76db8fc5a3
FearNoManButGod <85055501+FearNoManButGod@users.noreply.github.com>
2021-08-24 10:49:23 +0800

    Update jd_cash.js

--- jd_cash.js
+++ jd_cash.js
@@ -513,7 +513,7 @@ function shareCodesFormat() {
 			if( $.newShareCodes.indexOf(element) == -1){
 			  $.newShareCodes.push(element);
 			}
-		 }							   																						
+		 })							   																						
     } else {
       console.log(`由于您第${$.index}个京东账号未提供shareCode,将采纳本脚本自带的助力码\n`)
       const tempIndex = $.index > inviteCodes.length ? (inviteCodes.length - 1) : ($.index - 1);
