a5044d9dd6ce8ada8f7d1abf383c6b78766523ec
FearNoManButGod <85055501+FearNoManButGod@users.noreply.github.com>
2021-08-29 06:55:47 +0800

    Update crontab_list.sh

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -21,10 +21,6 @@
 0 0 * * * node /scripts/jd_dpqd.js >> /scripts/logs/jd_dpqd.log 2>&1
 #荣耀焕新季
 0 1 * * * node /scripts/jd_ryhxj.js >> /scripts/logs/jd_ryhxj.log 2>&1
-#手机狂欢
-0 0-18/6 9-28 8 *  node /scripts/jd_carnivalcity.js >> /scripts/logs/jd_carnivalcity.log 2>&1 
-#手机狂欢互助
-0 12,18 9-28 8 *  node /scripts/jd_carnivalcity_help.js >> /scripts/logs/jd_carnivalcity_help.log 2>&1 
 #活动路径  手机馆---》IQOO大牌日---〉左下角金机馆
 33 4,7 8-20 8 * node /scripts/jd_golden_machine.js >> /scripts/logs/jd_golden_machine.log 2>&1 
 #来电好物季
